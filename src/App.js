71b3b1e1761307faff5fea92039d1a32266f2822
hamid Hazzab <hamid.hazzab@lapiscine.pro>
2023-11-07 11:37:38 +0100

    add a composant named Footer

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
       <header className="App-header">
 
         <h1>Hello la piscine</h1>
-        <p>Exercice 1 je met un paragraphe pour l'exemple</p>
+        <p>Exercice 1 je met un paragraphe pour l'exemple et je push.</p>
        
-      
+       
         
       </header>
     </div>
---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer>
+        <p>exo 2 ceci est mon footer</p>
+        
+      </footer>
+    );
+  }
+  
+  export default Footer;
